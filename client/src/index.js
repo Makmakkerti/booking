23770092a86053406361a07253da31abec45fb64
Makmakkerti <makmakkerti007@gmail.com>
2022-06-28 16:31:58 +0300

    Basic reducers logic implemented

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { authReducer } from './reducers/auth';
-
-const rootReducer = combineReducers({
-	user: authReducer,
-});
+import { rootReducer } from './reducers/main';
 
 const store = createStore(rootReducer, composeWithDevTools());
 
