724381a5781df5d2d026e80e47631cde83e421c8
Makmakkerti <makmakkerti007@gmail.com>
2022-06-28 15:32:53 +0300

    Top navigation added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import TopNav from './conponents/TopNav';
 import Home from './booking/Home';
 import Login from './auth/Login';
 import Register from './auth/Register';
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>App started...</h1>
-		</div>
+		<BrowserRouter>
+			<TopNav />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/login' element={<Login />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
