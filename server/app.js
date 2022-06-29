88e43c805fb2488289912cfb31586d460c77e6d9
Makmakkerti <makmakkerti007@gmail.com>
2022-06-29 13:35:25 +0300

    Winston logger added

--- server/app.js
+++ server/app.js
@@ -1,13 +1,36 @@
 import express from 'express';
+import logger from './utils/logger.js';
 import path from 'path';
+import winston from 'winston';
 import { dirname } from 'path';
+import colors from 'colors';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+	const custom_format = winston.format.printf(
+		({ level, message, timestamp, ...metadata }) =>
+			`${colors.green(`[${level.toUpperCase()}]`)}: ${message}\n${
+				metadata && Object.keys(metadata).length ? JSON.stringify(metadata, null, 2) : ''
+			}`
+	);
+
+	logger.add(
+		new winston.transports.Console({
+			format: custom_format,
+		})
+	);
+}
 
 const app = express();
 
 // Healthcheck
 app.get('/', (req, res) => {
+	logger.info('Healthcheck');
 	res.status(201).send('App is running');
 });
 
